b74d220dc1cbf6fa90b2e3687de2e94d41e0e04a
facu-1 <facundolucerofiorelli@gmail.com>
2019-11-25 20:40:16 -0300

    header incluido en index.php

--- index.php
+++ index.php
@@ -14,67 +14,11 @@
 </head>
 
 <body class="container-fluid">
-  <!--Barra de navegacion-->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark rounded">
-    <a href="index.html" class="navbar-brand">
-      <img src="images/logo.svg" width="40" height="40" alt="logo" />
-      Marca
-    </a>
-    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item active">
-          <a href="" class="nav-link">Home</a>
-        </li>
-        <li class="nav-item">
-          <a href="" class="nav-link">FAQ</a>
-        </li>
-        <li class="nav-item">
-          <a href="" class="nav-link">Registro</a>
-        </li>
-        <li class="nav-item">
-          <a href="" class="nav-link">Login</a>
-        </li>
-        <li class="nav-item">
-          <a href="" class="nav-link">Contacto</a>
-        </li>
-      </ul>
-      <form class="form-inline my-2 my-lg-0 ml-auto">
-        <input class="form-control mr-sm-2" type="search" placeholder="Buscar" aria-label="Buscar" />
-        <button class="btn btn-outline-primary my-2 my-sm-0" type="submit">
-          Buscar
-        </button>
-      </form>
-    </div>
-  </nav>
-  <!--Fin de barra de navegacion-->
-  <!--Categorias-->
-  <nav class="nav nav-pills nav-fill d-inline justify-content-around">
-    <ul class="nav">
-      <li class="nav-item dropdown">
-        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Zapatillas</a>
-        <div class="dropdown-menu">
-          <a href="#" class="dropdown-item">Deportivas</a>
-          <a href="#" class="dropdown-item">De vestir</a>
-          <a href="#" class="dropdown-item">Trecking</a>
-        </div>
-      </li>
-      <li class="nav-item dropdown">
-        <a href="#" class="nav-link dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Indumentaria</a>
-        <div class="dropdown-menu">
-          <a href="#" class="dropdown-item">Ropa de invierno</a>
-          <a href="#" class="dropdown-item">Ropa de verano</a>
-          <a href="#" class="dropdown-item">Temporada 2019</a>
-        </div>
-      </li>
-      <li class="nav-item">
-        <a href="#" class="nav-link">Accesorios</a>
-      </li>
-    </ul>
-  </nav>
-  <!--Fin de categorias-->
+  <!--header -->
+  <header>
+    <?php include("header.php") ?>
+  </header>
+  <!-- fin header-->
   <!--Carrousel-->
   <div id="carrouselprincipal" class="carousel slide" data-ride="carousel">
     <ol class="carousel-indicators">
