96564520f7cbd1a2bcfc422f507bf7a427e81bb2
facu-1 <facundolucerofiorelli@gmail.com>
2019-11-25 20:35:06 -0300

    Incluido footer en index

--- footer.php
+++ footer.php
@@ -1,144 +1,128 @@
-<!DOCTYPE html>
-<html lang="es">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
- <title>Document</title>
-</head>
-<body>
-
 <div class="container">
 
-    <!-- Call to action -->
-    <ul class="list-unstyled list-inline text-center py-2">
-      <li class="list-inline-item">
-        <h5 class="mb-1">REGISTRATE</h5>
-      </li>
-      <li class="list-inline-item">
-        <a href="#!" class="btn btn-outline-white btn-rounded">¡Clic acá!</a>
-      </li>
-    </ul>
-    <!-- Call to action -->
-
-  </div>
-  <!-- Footer Elements -->
-
-
- 
-  <!-- Footer -->
-<footer class="page-footer font-small mdb-color lighten-3 pt-4">
-
-<!-- Footer Links -->
-<div class="container text-center text-md-left">
-
-  <!-- Grid row -->
-  <div class="row">
-
-    <!-- Grid column -->
-    <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
-
-      <!-- Content -->
-      <h5 class="font-weight-bold text-uppercase mb-4">Lorem</h5>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
-        esse
-        quasi, veritatis totam voluptas nostrum.</p>
-
-    </div>
-    <!-- Grid column -->
-
-    <hr class="clearfix w-100 d-md-none">
-
-    <!-- Grid column -->
-    <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
-
-      <!-- Links -->
-      <h5 class="font-weight-bold text-uppercase mb-4">PÁGINAS</h5>
-
-      <ul class="list-unstyled">
-        <li>
-          <p>
-            <a href="#!">Home</a>
-          </p>
-        </li>
-        <li>
-          <p>
-            <a href="#!">F.A.Q.</a>
-          </p>
-        </li>
-        <li>
-          <p>
-            <a href="#!">Contacto</a>
-          </p>
-        </li>
-      </ul>
-
-    </div>
-    <!-- Grid column -->
-
-    <hr class="clearfix w-100 d-md-none">
-
-    <!-- Grid column -->
-    <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
-
-      <!-- Contact details -->
-      <h5 class="font-weight-bold text-uppercase mb-4">DIRECCIÓN</h5>
-
-      <ul class="list-unstyled">
-        <li>
-          <p>
-            <i class="fas fa-home mr-3"></i> Mendoza, DH 2019, AR</p>
-        </li>
-        <li>
-          <p>
-            <i class="fas fa-envelope mr-3"></i> info@example.com</p>
-        </li>
-        <li>
-          <p>
-            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
-        </li>
-        <li>
-          <p>
-            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
-        </li>
-      </ul>
-
-    </div>
-    <!-- Grid column -->
-
-    <hr class="clearfix w-100 d-md-none">
-
-    <!-- Grid column -->
-    <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
-
-      <!-- Social buttons -->
-      <h5 class="font-weight-bold text-uppercase mb-4">SEGUINOS</h5>
-
-      <!-- Facebook -->
-      <a type="button" class="btn-floating btn-fb">
-        <i class="fab fa-facebook-f"></i>
-      </a>
-      <!-- Twitter -->
-      <a type="button" class="btn-floating btn-tw">
-        <i class="fab fa-twitter"></i>
-      </a>
-
-    </div>
-    <!-- Grid column -->
-
-  </div>
-  <!-- Grid row -->
+  <!-- Call to action -->
+  <ul class="list-unstyled list-inline text-center py-2">
+    <li class="list-inline-item">
+      <h5 class="mb-1">REGISTRATE</h5>
+    </li>
+    <li class="list-inline-item">
+      <a href="#!" class="btn btn-outline-white btn-rounded">¡Clic acá!</a>
+    </li>
+  </ul>
+  <!-- Call to action -->
 
 </div>
-<!-- Footer Links -->
+<!-- Footer Elements -->
+
+
+
+<!-- Footer -->
+<footer class="page-footer font-small mdb-color lighten-3 pt-4">
+
+  <!-- Footer Links -->
+  <div class="container text-center text-md-left">
+
+    <!-- Grid row -->
+    <div class="row">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mr-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Content -->
+        <h5 class="font-weight-bold text-uppercase mb-4">Lorem</h5>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit amet numquam iure provident voluptate
+          esse
+          quasi, veritatis totam voluptas nostrum.</p>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Links -->
+        <h5 class="font-weight-bold text-uppercase mb-4">PÁGINAS</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <a href="#!">Home</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">F.A.Q.</a>
+            </p>
+          </li>
+          <li>
+            <p>
+              <a href="#!">Contacto</a>
+            </p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-4 col-lg-3 mx-auto my-md-4 my-0 mt-4 mb-1">
+
+        <!-- Contact details -->
+        <h5 class="font-weight-bold text-uppercase mb-4">DIRECCIÓN</h5>
+
+        <ul class="list-unstyled">
+          <li>
+            <p>
+              <i class="fas fa-home mr-3"></i> Mendoza, DH 2019, AR</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+          </li>
+          <li>
+            <p>
+              <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+          </li>
+        </ul>
+
+      </div>
+      <!-- Grid column -->
+
+      <hr class="clearfix w-100 d-md-none">
+
+      <!-- Grid column -->
+      <div class="col-md-2 col-lg-2 text-center mx-auto my-4">
+
+        <!-- Social buttons -->
+        <h5 class="font-weight-bold text-uppercase mb-4">SEGUINOS</h5>
+
+        <!-- Facebook -->
+        <a type="button" class="btn-floating btn-fb">
+          <i class="fab fa-facebook-f"></i>
+        </a>
+        <!-- Twitter -->
+        <a type="button" class="btn-floating btn-tw">
+          <i class="fab fa-twitter"></i>
+        </a>
+
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+  </div>
+  <!-- Footer Links -->
 
 
 </footer>
 <!-- Footer -->
-</body>
-</html>
