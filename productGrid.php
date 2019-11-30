892e9c9e404ade8c2fefd6ce10832a4401a8ebe3
Nicolas Cohen <nicohen1998@gmail.com>
2019-11-30 15:58:58 -0300

    Grid de productos finalizado

---
+++ productGrid.php
@@ -0,0 +1,129 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+  <title>Sneakers</title>
+</head>
+
+<body>
+
+  <?php include_once('header.php'); ?>
+
+  <main role="main">
+
+    <section class="jumbotron text-center">
+      <div class="container">
+        <h1 class="jumbotron-heading">Sneakers</h1>
+        <p class="lead text-muted">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi, distinctio obcaecati. Ullam deserunt ea a.</p>
+        <p>
+          <a href="#" class="btn btn-primary my-2">Comprar</a>
+          <a href="#" class="btn btn-secondary my-2">Reviews</a>
+        </p>
+      </div>
+    </section>
+
+    <div class="album py-5 bg-light">
+      <div class="container">
+
+        <div class="row">
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center mb-5">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+          <div class="col-md-4">
+            <div class="card mb-4 box-shadow">
+              <img class="card-img-top" src="images\revolt-164_6wVEHfI-unsplash.jpg" alt="">
+            </div>
+            <div class="product-text text-center">
+              <h6>AIR FORCE 1 SAGE LOW</h6>
+              <p>$ 7.199</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </main>
+
+  <?php include_once("footer.php") ?>;
+
+  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+</body>
+
+</html>
