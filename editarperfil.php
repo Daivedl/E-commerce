daf8bf200099e66c0b093f8a432cec07a751b00d
facu-1 <facundolucerofiorelli@gmial.com>
2020-02-11 00:38:42 -0300

    Comienzo pag editar perfil

---
+++ editarperfil.php
@@ -0,0 +1,37 @@
+<?php
+session_start();
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <!-- head comun, incluye bootstrap, fav icons etc -->
+    <?php include_once("./resources/head.php"); ?>
+    <!-- fin head -->
+    <title>Perfil</title>
+</head>
+<!--header -->
+<header>
+    <?php include_once("./resources/header.php"); ?>
+</header>
+<!-- fin header-->
+
+
+
+
+
+
+
+
+<!--Inicio Footer -->
+<?php include_once("./resources/footer.php"); ?>
+<!--Fin Footer -->
+<!--scripts-->
+<?php include_once("./resources/scripts.php"); ?>
+<!-- fin scripts-->
+
+<body>
+
+</body>
+
+</html>
